cf629b68b5ff992a89cbcd9c447df7b35304f188
Koustav Maity <koustavm.aeie2018@nsec.ac.in>
2022-11-19 17:31:22 +0530

    Initial commit of all files to the repository

---
+++ counter-app/src/components/counters.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class counters extends Component {
+    render() { 
+        return (
+        <div>
+            <button
+             onClick={this.props.onReset} 
+             className="btn btn-primary btn-sm m-2">Reset</button>
+            {this.props.counters.map(counter =>( 
+            <Counter 
+            key={counter.id} 
+            onDelete={this.props.onDelete}
+            onIncrement = {this.props.onIncrement} 
+            counter={counter}>
+            </Counter>
+            )) }
+        </div>);
+    }
+}
+ 
+export default counters;
---
+++ counter-app/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class NavBar extends Component {
+    render() { 
+        return (<nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                NavBar <span className="badge badge-pill badge secondayr">{this.props.totalCounters}</span>
+            </a>
+        </nav>);
+    }
+}
+ 
+export default NavBar;
